09a606b3d875c83688c2204711eb9cd955092ac8
Alberto Peña Gómez <albertopg2001@gmail.com>
2025-05-12 21:04:32 +0200

    [UPDATE] Init tool window

--- Assets/Editor/MainWindow.cs
+++ Assets/Editor/MainWindow.cs
@@ -1,7 +1,18 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainWindow : EditorWindow
 {
     
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset;
+    
+    [MenuItem("Tools/Scriptvana")]
+    public static void ShowWindow()
+    {
+        MainWindow window = GetWindow<MainWindow>();
+        window.titleContent = new GUIContent("Scriptvana");
+        window.minSize = new Vector2(800, 600);
+    }
 }
